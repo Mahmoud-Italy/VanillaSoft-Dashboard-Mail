c1a39e87239dafba5e6c9ba0e97d2d6a5dfb07aa
Mahmoud.Ahmed <mahmoud.italy@outlook.com>
2021-02-27 13:51:58 +0100

    add jest testing

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,32 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import List from '@/views/mailable/List.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+describe(List, () => {
+  
+    let http;
+    let store;
+
+    beforeAll(() => {
+        http = new MockAdapter(axios);
+        store = new Vuex.Store(storeConfig());
+    });
+
+    afterEach(() => {
+        http.reset();
+    });
+
+    afterAll(() => {
+        http.restore();
+    });
+
+    it('calls inbox api and sets the flash messages', () => {
+        const fakeData = { 
+            api_token: ''
+         };
+        http.onPost('/v1/inbox').reply(200, { data: fakeData });
+        return store.dispatch('inbox')
+            .then(() => expect(store.state.messages).toHaveLength(1));
+    });
+  
 })
+
